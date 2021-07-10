b7214b3f2ae327d45438778e47ab4b3998a4f592
shantam8 <dscharmen@aol.com>
2021-07-10 14:16:55 +0200

    mobile + js done

--- js/main.js
+++ js/main.js
@@ -4,11 +4,28 @@ let divDarkoverlay = document.querySelector("#darkOverlay");
 
 let headerSection = document.querySelector("header");
 let btnToggleMobileMenu = document.querySelector("#btnToggleMobileMenu");
+let btnToggleBookmark = document.querySelector("#btnBookmarkProject");
 let sectionBackingModal = document.querySelector("#modalBackingProject");
+let sectionThankYouModal = document.querySelector("#modalThankYou");
 
-let btnSubmitBamboo = document.querySelector("#btnSubmitBamboo");
-let btnSubmitBlackEdition = document.querySelector("#btnSubmitBlackEdition");
-let btnSubmitMahogany = document.querySelector("#btnSubmitMahogany");
+// Select reward / open modal
+let btnSelectRewardBambooStand = document.querySelector(
+  "#btnSelectRewardBambooStand"
+);
+let btnSelectRewardBlackEdition = document.querySelector(
+  "#btnSelectRewardBlackStand"
+);
+let btnSelectRewardMahogany = document.querySelector(
+  "#btnSelectRewardMahogany"
+);
+
+// Submit selected reward bnt / close reward modal
+let btnModalSubmitNoReward = document.querySelector("#btnModalSubmitNoReward");
+let btnModalSubmitBamboo = document.querySelector("#btnModalSubmitBamboo");
+let btnModalSubmitBlackEdition = document.querySelector(
+  "#btnModalSubmitBlackEdition"
+);
+let btnModalSubmitMahogany = document.querySelector("#btnModalSubmitMahogany");
 
 let radioBtnBambooReward = document.querySelector("#radioBtnBambooReward");
 let radioBtnBlackEditionReward = document.querySelector(
@@ -21,13 +38,32 @@ let radioBtns = document.querySelectorAll('input[name="selectReward"]');
 let totalSumBacked = 89914;
 let totalNumberBackers = 5007;
 let totalNumberDaysLeft = 56;
-
 let statusFieldMoneyBacked = document.querySelector("#statsMoney");
 let statusFieldNumberBackers = document.querySelector("#statsBackers");
 let statusFieldNumberDaysLeft = document.querySelector("#statsDays");
 
+let totalRewardsLeftBamboo = 101;
+let totalRewardsLeftBlackEdition = 64;
+let totalRewardsLeftMahogany = 1;
+let statusFieldsRewardsLeftBamboo =
+  document.querySelectorAll(".rewardsLeftBamboo");
+let statusFieldsRewardsLeftBlackEdition = document.querySelectorAll(
+  ".rewardsLeftBlackEdition"
+);
+let statusFieldsRewardsLeftMahogany = document.querySelectorAll(
+  ".rewardsLeftMahogany"
+);
 
+function toggleBookmark(){
+  if(btnToggleBookmark.classList.contains("active")){
+    btnToggleBookmark.classList.remove("active");
 
+  }else{
+    btnToggleBookmark.classList.add("active");
+
+  }
+
+}
 
 function mobileMenuToggle() {
   if (headerSection.classList.contains("isOpen")) {
@@ -45,13 +81,6 @@ function mobileMenuToggle() {
   }
 }
 
-function deactivateRewardBlock(reward) {
-  let cardsToDeactivate = document.querySelectorAll("." + reward);
-  cardsToDeactivate.forEach((element) => {
-    element.classList.add("disabled");
-  });
-}
-
 function toggleModal(selectedReward) {
   if (sectionBackingModal.classList.contains("showModal")) {
     sectionBackingModal.classList.remove("showModal");
@@ -60,7 +89,38 @@ function toggleModal(selectedReward) {
   } else {
     sectionBackingModal.classList.add("showModal");
     divDarkoverlay.classList.add("darkoverlay");
+    window.scrollTo(0,0);
   }
+}
+
+function toggleShowThankYouModal() {
+  if (sectionThankYouModal.classList.contains("showModal")) {
+    sectionThankYouModal.classList.remove("showModal");
+    divDarkoverlay.classList.remove("darkoverlay");
+    toggleRadioBtn("allOff");
+  } else {
+    sectionBackingModal.classList.remove("showModal");
+    sectionThankYouModal.classList.add("showModal");
+    window.scrollTo(0,0);
+  }
+}
+
+function processSubmittedPledge(reward, value) {
+  switch (reward) {
+    case "bambooStand":
+      totalRewardsLeftBamboo--;
+      break;
+    case "blackEdition":
+      totalRewardsLeftBlackEdition--;
+      break;
+    case "mahogany":
+      totalRewardsLeftMahogany--;
+      break;
+  }
+  totalSumBacked = totalSumBacked + parseInt(value);
+  totalNumberBackers++;
+  updateBackerStates();
+  updateRewardsLeft();
 }
 
 function toggleRadioBtn(selectedRadioBtn) {
@@ -74,17 +134,53 @@ function toggleRadioBtn(selectedRadioBtn) {
   });
 }
 
+function deactivateRewardBlock(reward) {
+  let cardsToDeactivate = document.querySelectorAll("." + reward);
+  cardsToDeactivate.forEach((element) => {
+    element.classList.add("disabled");
+    element.querySelector("button").innerText = "Out of Stock";
+  });
+}
+
 function updateBackerStates() {
+  if (totalSumBacked <= 100000) {
+    document.querySelector(".progressbarFilled").style.width =
+      totalSumBacked / 1000 + "%";
+  } else {
+    document.querySelector(".progressbarFilled").style.width = "100%";
+  }
+  statusFieldMoneyBacked.innerText = "$" + totalSumBacked.toLocaleString();
+  statusFieldNumberBackers.innerText = totalNumberBackers.toLocaleString();
+  statusFieldNumberDaysLeft.innerText = totalNumberDaysLeft;
+}
 
-  statusFieldMoneyBacked.innerText = "$"+(totalSumBacked.toLocaleString());
-statusFieldNumberBackers.innerText = totalNumberBackers.toLocaleString();
-statusFieldNumberDaysLeft.innerText = totalNumberDaysLeft;
+function updateRewardsLeft() {
+  statusFieldsRewardsLeftBamboo.forEach((element) => {
+    element.innerText = totalRewardsLeftBamboo;
+  });
+  statusFieldsRewardsLeftBlackEdition.forEach((element) => {
+    element.innerText = totalRewardsLeftBlackEdition;
+  });
+  statusFieldsRewardsLeftMahogany.forEach((element) => {
+    element.innerText = totalRewardsLeftMahogany;
+  });
 
+  if (totalRewardsLeftBamboo == 0) {
+    deactivateRewardBlock("bambooStand");
+  }
+  if (totalRewardsLeftBlackEdition == 0) {
+    deactivateRewardBlock("blackEdition");
+  }
+  if (totalRewardsLeftMahogany == 0) {
+    deactivateRewardBlock("mahogany");
+  }
 }
 
 function init() {
   console.log("Hi");
   updateBackerStates();
+  updateRewardsLeft();
+  toggleRadioBtn("allOff");
 
   radioBtns.forEach((element) => {
     element.addEventListener("change", () => {
@@ -93,31 +189,57 @@ function init() {
   });
 
   btnToggleMobileMenu.addEventListener("click", mobileMenuToggle);
+  btnToggleBookmark.addEventListener("click", toggleBookmark);
 
   document
     .querySelector("#btnCloseModal")
     .addEventListener("click", toggleModal);
-
   document
     .querySelector("#btnBackProject")
     .addEventListener("click", toggleModal);
-
-  document
-    .querySelector("#btnRewardBambooStand")
-    .addEventListener("click", () => {
-      toggleModal("bambooStand");
-    });
-
-  document
-    .querySelector("#btnRewardBlackStand")
-    .addEventListener("click", () => {
-      toggleModal("blackEdition");
-    });
-  document.querySelector("#btnRewardMahogany").addEventListener("click", () => {
+  btnSelectRewardBambooStand.addEventListener("click", () => {
+    toggleModal("bambooStand");
+  });
+  btnSelectRewardBlackEdition.addEventListener("click", () => {
+    toggleModal("blackEdition");
+  });
+  btnSelectRewardMahogany.addEventListener("click", () => {
     toggleModal("mahogany");
   });
-  toggleRadioBtn("allOff");
-  //deactivateRewardBlock("mahogany");
+
+  btnModalSubmitNoReward.addEventListener("click", () => {
+    toggleShowThankYouModal();
+    processSubmittedPledge(
+      "noReward",
+      document.querySelector("#sumForNoReward").value
+    );
+  });
+
+  btnModalSubmitBamboo.addEventListener("click", () => {
+    toggleShowThankYouModal();
+    processSubmittedPledge(
+      "bambooStand",
+      document.querySelector("#sumForBamboo").value
+    );
+  });
+  btnModalSubmitBlackEdition.addEventListener("click", () => {
+    toggleShowThankYouModal();
+    processSubmittedPledge(
+      "blackEdition",
+      document.querySelector("#sumForBlackEdition").value
+    );
+  });
+  btnModalSubmitMahogany.addEventListener("click", () => {
+    toggleShowThankYouModal();
+    processSubmittedPledge(
+      "mahogany",
+      document.querySelector("#sumForMahogany").value
+    );
+  });
+
+  document
+    .querySelector("#btnThankYouModal")
+    .addEventListener("click", toggleShowThankYouModal);
 }
 
 window.onload = init;
